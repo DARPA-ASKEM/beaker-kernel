50e4c53c5bdda737f1fa97fa088e2668f6cbf210
Five Grant <5@fivegrant.com>
2023-11-30 13:53:32 -0600

    Reinclude SVG with fix

--- contexts/codesets/decapodes/julia-1.9/expr_to_info.jl
+++ contexts/codesets/decapodes/julia-1.9/expr_to_info.jl
@@ -2,19 +2,14 @@ using SyntacticModels, Decapodes, Catlab
 import JSON3, DisplayAs
 
 function expr_to_svg(model)
-    _path = "/tmp/decapode.json"
-    open(_path, "w") do f
-        io = IOBuffer()
-        Catlab.Graphics.Graphviz.run_graphviz(io, to_graphviz(model), format="svg")
-    end
-    open(_path, "r") do f
-        return String(take!(f))
-    end
+    io = IOBuffer()
+    Catlab.Graphics.Graphviz.run_graphviz(io, to_graphviz(model), format="svg")
+    io |> String ∘ take! 
 end
 
 _response = Dict(
-    "application/json" => generate_json_acset({{ target }}),
-    # "image/svg" => expr_to_svg({{ target }}) # TODO: Reinclude when graphviz bug is fixed
+    "application/json" => (generate_json_acset ∘ expand_operators)({{ target }}),
+    "image/svg" => expr_to_svg({{ target }}) # TODO: Reinclude when graphviz bug is fixed
 )
 
 
