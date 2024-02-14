373eab7f264c01b71b55580e4101400ee5b64cb3
Five Grant <5@fivegrant.com>
2024-02-14 09:28:35 -0600

    Use new imports

--- src/askem_beaker/contexts/decapodes/procedures/julia/setup.jl
+++ src/askem_beaker/contexts/decapodes/procedures/julia/setup.jl
@@ -1,7 +1,8 @@
-using ACSets
 using Catlab, Catlab.Graphics
 using CombinatorialSpaces
 using Decapodes
+using DiagrammaticEquations
+using DiagrammaticEquations.Deca
 
 _model_reset_cache = Dict()
 decapode = @decapode begin end
