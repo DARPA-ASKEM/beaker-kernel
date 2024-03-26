9d1a605ccf50c2aa534f2464886dfdb9028a9e27
Five Grant <5@fivegrant.com>
2024-03-26 17:06:22 -0400

    Lookup functions again

--- src/askem_beaker/contexts/mimi/procedures/julia/info.jl
+++ src/askem_beaker/contexts/mimi/procedures/julia/info.jl
@@ -4,7 +4,7 @@ _IGNORED_SYMBOLS = [:Base, :Core, :InteractiveUtils, :Main]
 _from_module = {{from_module | default("true") }}
 _return_source = {{ return_source | default("false") }} # TODO: Use this with CodeTracking.jl
 _selected_module = :{{ module | default("nothing") }}
-_chosen_funcs_str = {{ function_names | default("[]") }}
+_chosen_funcs_str = "{{ function_names | default("") }}"
 
 
 _function_names = 
@@ -13,16 +13,16 @@ _function_names =
             if !isnothing(_selected_module)
                 filter(x -> in(x, _IGNORED_SYMBOLS) && isa(x, Module), names(Main))
             else
-                _selected_module
+                [_selected_module]
             end
-        filter(!in(_module_names), reduce(vcat, [names(mod) for mod in _module_names]; init=[]))
+        filter(!in(_module_names), reduce(vcat, [names(eval(mod); all=true) for mod in _module_names]; init=[]))
     else
-        split(_chosen_func_str, ",")
+        Symbol.(split(_chosen_funcs_str, ","))
     end
 
 _docs = Dict{Symbol, Any}()
 for func in _function_names
-    _docs[func] = eval(:(@doc $func))
+    _docs[func] = string(eval(:(@doc $func)))
 end
 
 _docs |> DisplayAs.unlimited ∘ JSON3.write
