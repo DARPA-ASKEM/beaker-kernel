a609557efd26aa1a436e03fe67a7f523f786f0ae
Tom Szendrey <T.Szendrey@hotmail.com>
2024-05-07 16:38:18 -0400

    Need to test changes but my local is fighting

--- src/askem_beaker/contexts/mira_model_edit/procedures/python3/replace_ratelaw.py
+++ src/askem_beaker/contexts/mira_model_edit/procedures/python3/replace_ratelaw.py
@@ -1,10 +1,8 @@
-def replace_rate_law_sympy(model, template_name: str, new_rate_law, local_dict=None):
+def replace_rate_law_sympy(template_name, new_rate_law):
     """Replace the rate law of transition. The new rate law passed in will be a sympy.Expr object
 
     Parameters
     ----------
-    model :
-        The model as an AMR JSON
     template_name :
         The ID of the transition whose rate law is to be replaced, this is
         typically the name of the transition
@@ -22,7 +20,7 @@ def replace_rate_law_sympy(model, template_name: str, new_rate_law, local_dict=N
     tm = model
     for template in tm.templates:
         if template.name == template_name:
-            template.set_rate_law(rate_law, local_dict=local_dict)
+            template.set_rate_law(new_rate_law, local_dict=None)
     return tm
 
-model = replace_rate_law_sympy(model, "{{ template_name }}", "{{ new_rate_law }}")
+model = replace_rate_law_sympy("{{ template_name }}", "{{ new_rate_law }}")
